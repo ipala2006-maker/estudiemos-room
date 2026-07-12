a84ecb3597330a7f1a9901b4f451f1e2c990eec9
ipala2006-maker <ipala2006@gmail.com>
2026-07-12 15:12:32 -0300

    chore: trigger pages deploy for keyboard arrows

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = 'Pages build 2026-07-12 15:09 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-12-1509-keyboard-arrows';
+export const BUILD_LABEL = 'Pages build 2026-07-12 15:12 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-12-1512-keyboard-arrows';
