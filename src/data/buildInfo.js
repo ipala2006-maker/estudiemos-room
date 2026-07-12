ac6eb0b41998e67ff53c56cc2c26496b5a915610
ipala2006-maker <ipala2006@gmail.com>
2026-07-12 15:03:47 -0300

    chore: trigger pages deploy for spotify agenda polish

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = 'Pages build 2026-07-12 15:00 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-12-1500-spotify-agenda-polish';
+export const BUILD_LABEL = 'Pages build 2026-07-12 15:03 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-12-1503-spotify-agenda-polish';
