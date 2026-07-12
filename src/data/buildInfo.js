af96e63811eeeef44a413a328c8686012620a27f
ipala2006-maker <ipala2006@gmail.com>
2026-07-12 14:23:15 -0300

    chore: trigger pages deploy for agenda ux

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = 'Pages build 2026-07-12 14:20 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-12-1420-agenda-ux';
+export const BUILD_LABEL = 'Pages build 2026-07-12 14:23 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-12-1423-agenda-ux';
