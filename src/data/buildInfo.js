11f202903bed517f11b2fd159f1adec7bcd24f7f
ipala2006-maker <ipala2006@gmail.com>
2026-07-12 12:18:58 -0300

    chore: trigger pages deploy for focus economy

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = 'Pages build 2026-07-12 12:15 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-12-1215-focus-economy';
+export const BUILD_LABEL = 'Pages build 2026-07-12 12:18 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-12-1218-focus-economy';
