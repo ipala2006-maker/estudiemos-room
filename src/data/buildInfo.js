7e32b1a44b24d858d67387c10f666c595ee76685
ipala2006-maker <ipala2006@gmail.com>
2026-07-12 01:15:42 -0300

    Trigger Pages for compact guide and mascot update

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = 'Pages build 2026-07-12 01:10 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-12-0110';
+export const BUILD_LABEL = 'Pages build 2026-07-12 01:14 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-12-0114';
