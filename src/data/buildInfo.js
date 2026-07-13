f8d7f71903c1e56000f4bd60fac35e3fdbfcff8a
ipala2006-maker <ipala2006@gmail.com>
2026-07-13 13:02:31 -0300

    chore: trigger pages deploy for reliable backspace screen

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = 'Pages build 2026-07-13 13:09 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-13-1309-backspace-fallback-32-9-screen';
+export const BUILD_LABEL = 'Pages build 2026-07-13 13:12 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-13-1312-backspace-fallback-32-9-screen';
