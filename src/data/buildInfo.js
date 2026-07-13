66ac7329ab9e9689a27501d15a21b6bdab846b5d
ipala2006-maker <ipala2006@gmail.com>
2026-07-13 12:41:42 -0300

    chore: trigger pages deploy for backspace screen update

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = 'Pages build 2026-07-13 12:46 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-13-1246-backspace-home-larger-screen';
+export const BUILD_LABEL = 'Pages build 2026-07-13 12:48 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-13-1248-backspace-home-larger-screen';
