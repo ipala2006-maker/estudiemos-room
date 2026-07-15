3a2d90ee241b06b6f826a70d88f5236c693d6d83
ipala2006-maker <ipala2006@gmail.com>
2026-07-15 13:50:29 -0300

    chore: publish taskbar HUD polish to Pages

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = 'Pages build 2026-07-15 13:46 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1346-taskbar-hud-polish-pages';
+export const BUILD_LABEL = 'Pages build 2026-07-15 13:50 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1350-taskbar-hud-polish-pages';
