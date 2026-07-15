7e5ba0772b3fee86290dd28be4fc2a4386f76d4b
ipala2006-maker <ipala2006@gmail.com>
2026-07-15 13:38:14 -0300

    chore: publish taskbar bottom grid fix to Pages

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = 'Pages build 2026-07-15 13:36 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1336-taskbar-bottom-grid-fix-pages';
+export const BUILD_LABEL = 'Pages build 2026-07-15 13:42 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1342-taskbar-bottom-grid-fix-pages';
