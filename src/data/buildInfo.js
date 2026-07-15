2aaf27339b3a8278ee1c50eba77b3e5bbe487dfc
ipala2006-maker <ipala2006@gmail.com>
2026-07-15 18:34:23 -0300

    chore: publish profile keyboard scroll marker

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,6 +1,2 @@
-import '../styles/computer-keyboard-controls.css';
-import '../styles/computer-keyboard-scroll.css';
-import '../utils/installComputerKeyboardController.js';
-
-export const BUILD_LABEL = 'Pages build 2026-07-15 15:32 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1532-computer-backspace-scroll-pages';
+export const BUILD_LABEL = 'Pages build 2026-07-15 15:55 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1555-profile-keyboard-scroll-pages';
