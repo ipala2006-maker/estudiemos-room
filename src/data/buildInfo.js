09f07a9237ec729b766e7d4e6e2b10e60d300a59
ipala2006-maker <ipala2006@gmail.com>
2026-07-15 14:49:51 -0300

    chore: publish computer keyboard navigation marker

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,5 @@
-export const BUILD_LABEL = 'Pages build 2026-07-15 13:58 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1358-taskbar-hud-polish-pages';
+import '../styles/computer-keyboard-controls.css';
+import '../utils/installComputerKeyboardController.js';
+
+export const BUILD_LABEL = 'Pages build 2026-07-15 14:37 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1437-computer-keyboard-nav-pages';
