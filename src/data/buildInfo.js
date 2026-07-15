ed3c064129ff924406283539a5b9580ce330aa4b
ipala2006-maker <ipala2006@gmail.com>
2026-07-15 12:47:55 -0300

    chore: publish computer window fix to Pages

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = 'Pages build 2026-07-15 12:45 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1245-computer-window-fix-pages';
+export const BUILD_LABEL = 'Pages build 2026-07-15 12:50 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1250-computer-window-fix-pages';
