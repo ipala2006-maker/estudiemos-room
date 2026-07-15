5e8664564829ba4dff53c16cbe783214d433e4e7
ipala2006-maker <ipala2006@gmail.com>
2026-07-15 12:28:39 -0300

    chore: publish Windows 11 computer UI to Pages

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = 'Pages build 2026-07-15 12:23 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1223-windows11-computer-ui';
+export const BUILD_LABEL = 'Pages build 2026-07-15 12:27 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1227-windows11-computer-ui-pages';
