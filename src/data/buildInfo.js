19f29b8277c0e540c5acdd1d2c309dbf8604c8fa
ipala2006-maker <ipala2006@gmail.com>
2026-07-15 14:24:56 -0300

    chore: publish HUD polish marker

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,2 +1,2 @@
-export const BUILD_LABEL = 'Pages build 2026-07-15 13:50 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1350-taskbar-hud-polish-pages';
+export const BUILD_LABEL = 'Pages build 2026-07-15 13:58 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1358-taskbar-hud-polish-pages';
