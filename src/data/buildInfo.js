9ffb161be29c26d229cfd4313fc8b05184fbdad5
ipala2006-maker <ipala2006@gmail.com>
2026-07-15 20:29:44 -0300

    Enable room speaker on Pages

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,5 +1,7 @@
 import '../styles/spotify-playback-enhancer.css';
+import '../styles/speaker-remote.css';
+import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-15 19:24 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-1924-spotify-playback-api-pages';
+export const BUILD_LABEL = 'Pages build 2026-07-15 20:06 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-2006-room-speaker-spotify-pages';
