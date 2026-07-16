d8711a9cd982a9647c052ed9964917e01e70187f
ipala2006-maker <ipala2006@gmail.com>
2026-07-16 12:02:46 -0300

    Revert noisy HUD polish

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -3,5 +3,5 @@ import '../styles/speaker-remote.css';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-15 23:21 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-2321-entry-hud-polish-pages';
+export const BUILD_LABEL = 'Pages build 2026-07-16 00:24 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-0024-quiet-hud-revert-pages';
