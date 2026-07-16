c2244fa8d642c87e5a8c179c0b1a5fc588c30910
ipala2006-maker <ipala2006@gmail.com>
2026-07-16 14:36:34 -0300

    Mark speaker square and clean HUD build

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -3,5 +3,5 @@ import '../styles/speaker-remote.css';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-16 13:22 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-1322-visible-room-speaker-pages';
+export const BUILD_LABEL = 'Pages build 2026-07-16 14:08 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-1408-speaker-square-clean-entry';
