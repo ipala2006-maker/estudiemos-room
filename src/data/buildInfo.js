42edec4660f4963544a51975886225b68bb9fcd9
ipala2006-maker <ipala2006@gmail.com>
2026-07-16 20:55:51 -0300

    Update build marker for HUD polish

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -3,5 +3,5 @@ import '../styles/speaker-remote.css';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-16 17:55 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-1755-speaker-hidden-in-lobby';
+export const BUILD_LABEL = 'Pages build 2026-07-16 20:51 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-2051-hud-polish';
