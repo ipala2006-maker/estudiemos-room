859d799c9c66f5e96a9b699708531b0e567ca973
ipala2006-maker <ipala2006@gmail.com>
2026-07-16 04:43:14 -0300

    Polish start screen and compact HUD

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -3,5 +3,5 @@ import '../styles/speaker-remote.css';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-15 21:56 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-2156-direct-room-speaker-pages';
+export const BUILD_LABEL = 'Pages build 2026-07-15 23:21 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-15-2321-entry-hud-polish-pages';
