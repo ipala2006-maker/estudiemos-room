008c35ed27f8881f45e8b297f3449aa58e9151cc
ipala2006-maker <ipala2006@gmail.com>
2026-07-16 13:34:30 -0300

    Mark visible room speaker Pages build

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -3,5 +3,5 @@ import '../styles/speaker-remote.css';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-16 12:07 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-1207-clean-hud-copy-pages';
+export const BUILD_LABEL = 'Pages build 2026-07-16 13:22 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-1322-visible-room-speaker-pages';
