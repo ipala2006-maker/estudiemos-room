98a99355f6668195bb490c1ea309796a438b52ae
ipala2006-maker <ipala2006@gmail.com>
2026-07-16 17:48:48 -0300

    Refresh Pages marker for opaque speaker

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -3,5 +3,5 @@ import '../styles/speaker-remote.css';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-16 17:24 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-1724-speaker-corner-position';
+export const BUILD_LABEL = 'Pages build 2026-07-16 17:45 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-1745-speaker-css-occluder';
