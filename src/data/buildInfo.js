218312ab762228ca8b5b6ec4a87c4936084d2c66
ipala2006-maker <ipala2006@gmail.com>
2026-07-16 17:58:54 -0300

    Refresh Pages marker for lobby speaker visibility

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -3,5 +3,5 @@ import '../styles/speaker-remote.css';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-16 17:45 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-1745-speaker-css-occluder';
+export const BUILD_LABEL = 'Pages build 2026-07-16 17:55 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-1755-speaker-hidden-in-lobby';
