5667600d015febc527186bba6062a5e9e4325cda
ipala2006-maker <ipala2006@gmail.com>
2026-07-16 15:23:02 -0300

    Refresh Pages build marker for speaker scene

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -3,5 +3,5 @@ import '../styles/speaker-remote.css';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-16 14:08 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-1408-speaker-square-clean-entry';
+export const BUILD_LABEL = 'Pages build 2026-07-16 14:52 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-1452-speaker-scene-anchor-verified';
