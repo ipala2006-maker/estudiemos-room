8dad63ae65095b949270d7d629ab99a4ed010f61
ipala2006-maker <ipala2006@gmail.com>
2026-07-17 18:32:01 -0300

    Enable opaque hand overlay build

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,8 +1,9 @@
 import '../styles/spotify-playback-enhancer.css';
 import '../styles/speaker-remote.css';
 import '../utils/installInteractionTargeting.js';
+import '../utils/installOpaqueHandOverlay.js';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-17 18:05 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-17-1805-agenda-direct-aim-only';
+export const BUILD_LABEL = 'Pages build 2026-07-17 18:23 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-17-1823-opaque-hand-overlay';
