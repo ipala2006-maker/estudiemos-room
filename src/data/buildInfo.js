b77315fac5c9f5d755a93d825f6fc6f41ea929f3
ipala2006-maker <ipala2006@gmail.com>
2026-07-17 13:25:44 -0300

    Update Pages build marker for agenda signs pass

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -4,5 +4,5 @@ import '../utils/installInteractionTargeting.js';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-16 23:18 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-2318-visible-runtime-fix';
+export const BUILD_LABEL = 'Pages build 2026-07-17 12:59 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-17-1259-agenda-signs-spacing-fix';
