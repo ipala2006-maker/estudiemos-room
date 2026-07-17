68144d9420604a41ec5737d9a47ecf60ff73e7b9
ipala2006-maker <ipala2006@gmail.com>
2026-07-16 21:29:05 -0300

    Update build marker for interaction targeting

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,7 +1,8 @@
 import '../styles/spotify-playback-enhancer.css';
 import '../styles/speaker-remote.css';
+import '../utils/installInteractionTargeting.js';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-16 20:51 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-2051-hud-polish';
+export const BUILD_LABEL = 'Pages build 2026-07-16 21:16 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-2116-interaction-focus';
