dad2e673c2605695f2769a87f21b680feaa24e3e
ipala2006-maker <ipala2006@gmail.com>
2026-07-17 14:09:54 -0300

    Update Pages marker for clean signs pass

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -4,5 +4,5 @@ import '../utils/installInteractionTargeting.js';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-17 13:49 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-17-1349-agenda-signs-recursion-fix';
+export const BUILD_LABEL = 'Pages build 2026-07-17 14:08 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-17-1408-clean-signs-wide-neighborhood';
