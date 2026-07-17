b7ff4c91cbbda63a81c829df3047d7ab4b306130
ipala2006-maker <ipala2006@gmail.com>
2026-07-17 13:52:10 -0300

    Update Pages marker for recursion fix

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -4,5 +4,5 @@ import '../utils/installInteractionTargeting.js';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-17 13:32 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-17-1332-agenda-signs-scene-hook-fix';
+export const BUILD_LABEL = 'Pages build 2026-07-17 13:49 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-17-1349-agenda-signs-recursion-fix';
