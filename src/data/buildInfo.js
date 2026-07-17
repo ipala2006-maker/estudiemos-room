2bc13ddd3efec239b02cec378896b45788bb9104
ipala2006-maker <ipala2006@gmail.com>
2026-07-16 22:37:29 -0300

    Update visible build marker for agenda and neighborhood fix

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -4,5 +4,5 @@ import '../utils/installInteractionTargeting.js';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-16 21:41 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-2141-agenda-target-fix';
+export const BUILD_LABEL = 'Pages build 2026-07-16 22:32 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-2232-agenda-neighborhood-fix';
