379ff5f066787371db2b183625c6781b5922c387
ipala2006-maker <ipala2006@gmail.com>
2026-07-17 18:53:33 -0300

    Mark hand original size build

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -5,5 +5,5 @@ import '../utils/installOpaqueHandOverlay.js';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-17 18:23 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-17-1823-opaque-hand-overlay';
+export const BUILD_LABEL = 'Pages build 2026-07-17 18:50 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-17-1850-hand-original-size';
