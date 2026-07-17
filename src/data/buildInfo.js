e6df598ba65101724e3d0c624cbbe9720395c4c1
ipala2006-maker <ipala2006@gmail.com>
2026-07-16 21:44:16 -0300

    Update build marker for agenda target fix

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -4,5 +4,5 @@ import '../utils/installInteractionTargeting.js';
 import '../utils/installRoomSpeakerWorld.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-16 21:16 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-2116-interaction-focus';
+export const BUILD_LABEL = 'Pages build 2026-07-16 21:41 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-16-2141-agenda-target-fix';
