7538944144cb385dacf5bd63b5a4699c98bfd490
ipala2006-maker <ipala2006@gmail.com>
2026-07-18 13:59:06 -0300

    Mark wall agenda soft UI build

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,5 +1,6 @@
 import '../styles/spotify-playback-enhancer.css';
 import '../styles/speaker-remote.css';
+import '../styles/wall-agenda-soft-ui.css';
 import '../utils/installInteractionTargeting.js';
 import '../utils/installOpaqueHandOverlay.js';
 import '../utils/installRoomSpeakerWorld.js';
@@ -7,5 +8,5 @@ import '../utils/installSpeakerInteractionTargetFix.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 import '../utils/installLiveEstudiemosSync.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-18 13:09 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-18-1309-remove-hud-rank-chip';
+export const BUILD_LABEL = 'Pages build 2026-07-18 13:50 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-18-1350-wall-agenda-soft-ui';
