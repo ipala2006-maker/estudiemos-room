df8e027c41c4b09d682ea538db44fb4e82d06d25
ipala2006-maker <ipala2006@gmail.com>
2026-07-18 20:15:21 -0300

    Mark PC agenda design build

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -2,6 +2,7 @@ import '../styles/spotify-playback-enhancer.css';
 import '../styles/speaker-remote.css';
 import '../styles/wall-agenda-soft-ui.css';
 import '../styles/wall-agenda-keyboard-polish.css';
+import '../styles/wall-agenda-pc-design.css';
 import '../styles/agenda-planner.css';
 import '../utils/installInteractionTargeting.js';
 import '../utils/installOpaqueHandOverlay.js';
@@ -12,5 +13,5 @@ import '../utils/installLiveEstudiemosSync.js';
 import '../utils/installWallAgendaKeyboardPolish.js';
 import '../utils/installComputerAgendaCleanup.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-18 19:54 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-18-1954-unified-empty-agenda';
+export const BUILD_LABEL = 'Pages build 2026-07-18 20:14 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-18-2014-pc-agenda-design';
