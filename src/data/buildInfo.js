2f7b26d06bf89aab436b4595f4d9fcafa8fbe37a
ipala2006-maker <ipala2006@gmail.com>
2026-07-18 16:53:37 -0300

    Mark polished agenda build

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -1,6 +1,7 @@
 import '../styles/spotify-playback-enhancer.css';
 import '../styles/speaker-remote.css';
 import '../styles/wall-agenda-soft-ui.css';
+import '../styles/agenda-planner.css';
 import '../utils/installInteractionTargeting.js';
 import '../utils/installOpaqueHandOverlay.js';
 import '../utils/installRoomSpeakerWorld.js';
@@ -8,5 +9,5 @@ import '../utils/installSpeakerInteractionTargetFix.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 import '../utils/installLiveEstudiemosSync.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-18 14:20 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-18-1420-wall-agenda-simple-entry';
+export const BUILD_LABEL = 'Pages build 2026-07-18 16:15 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-18-1615-agenda-clickup-polish';
