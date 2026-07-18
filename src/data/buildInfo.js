059fc88d41d9eb66d0575e54ef5374236d8ab58e
ipala2006-maker <ipala2006@gmail.com>
2026-07-18 14:34:57 -0300

    Mark simple wall agenda build

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -8,5 +8,5 @@ import '../utils/installSpeakerInteractionTargetFix.js';
 import '../utils/installSpotifyPlaybackEnhancer.js';
 import '../utils/installLiveEstudiemosSync.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-18 14:05 ART';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-18-1405-wall-agenda-entry-ux';
+export const BUILD_LABEL = 'Pages build 2026-07-18 14:20 ART';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-18-1420-wall-agenda-simple-entry';
