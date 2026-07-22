9a57ba93360a465cc805c3d4634cdbb5f5a9e1e7
ipala2006-maker <ipala2006@gmail.com>
2026-07-22 16:14:25 -0300

    Mark compact shop build

--- src/data/buildInfo.js
+++ src/data/buildInfo.js
@@ -19,5 +19,5 @@ import '../utils/installComputerAgendaCleanup.js';
 import '../utils/installRoomShopWorld.js';
 import '../utils/installRoomShopWorldPolishFix.js';
 
-export const BUILD_LABEL = 'Pages build 2026-07-22 shop polish fix';
-export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-22-shop-ui-physical-fix-v3';
+export const BUILD_LABEL = 'Pages build 2026-07-22 shop compact polish';
+export const BUILD_MARKER = 'pages-visible-build-marker-2026-07-22-shop-ui-compact-fix-v4';
